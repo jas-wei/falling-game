0ef5d67b4e59f3eb7c45ac4090b450a72638b64a
Jasmine Wei <144985291+jas-wei@users.noreply.github.com>
2024-10-26 12:14:06 -0700

    spawning works better now

--- main_game.gd
+++ main_game.gd
@@ -11,5 +11,5 @@ func _ready():
 func _on_timer_timeout() -> void:
 	print("asdas")
 	var new_rigid_body = rigid_body_scene.instantiate()  # Create an instance of the RigidBody2D scene
-	new_rigid_body.position = Vector2(100, 8)            # Set the spawn position
+	new_rigid_body.position = Vector2(randi_range(-1000, 1000), -300)            # Set the spawn position
 	add_child(new_rigid_body)                            # Add the RigidBody2D to the scene tree
